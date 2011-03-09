a73af6701b1520e24e90424b7e33735417dc6c34
Yadu Nand B <yadudoc1729@gmail.com>
2011-03-10 01:41:50 +0530

    modified:   rb_tree.erl

--- rb_tree.erl
+++ rb_tree.erl
@@ -28,28 +28,38 @@ ins( null, Item ) ->
 ins([{Item ,C}, Left, Right], Item ) ->
     [{Item ,C}, Left, Right];
 ins([{N,C}, Left, Right], Item ) when Item < N ->
-    balance( [{N,C}, ins(Left, Item), Right], Item);
+    case1( [{N,C}, ins(Left, Item), Right], Item);
 ins([{N,C}, Left, Right], Item ) when Item > N ->
-    balance( [{N,C}, Left, ins(Right, Item)], Item).
+    case1( [{N,C}, Left, ins(Right, Item)], Item).
 
 root_sanity([{Root, _}, Left, Right]) ->
     [{Root, black}, Left, Right];
 root_sanity(null) ->
     null.
     
-
-case1([{G,Gc}, [{L,red}, [{Item, red}, Lll, Llr], Lr ],[{R,red},Rl,Rr]], 
+% Case 1 
+% The parent and the newly created node is red.
+% If the sibling of the parent i.e the uncle is also red
+% then, all we need to do is change the color of the parent
+% and uncle to black and that of the grandparent to red.
+case1([{G,_}, 
+       [{L,red}, [{Item, red}, Lll, Llr], Lr ], 
+       [{R,red},Rl,Rr]], 
 	Item) ->
-    [{G,Gc}, [{L,black}, [{Item, red}, Lll, Llr],Lr], [{R,black},Rl,Rr]];
-case1([{G,Gc}, [{L,red}, Ll, [{Item, red}, Lrl, Lrr]],[{R,red},Rl,Rr]],
+    [{G,red}, [{L,black}, [{Item, red}, Lll, Llr],Lr], [{R,black},Rl,Rr]];
+case1([{G,_}, 
+       [{L,red}, Ll, [{Item, red}, Lrl, Lrr]],
+       [{R,red},Rl,Rr]],
 	Item) ->
-    [{G,Gc}, [{L,black}, Ll, [{Item, red}, Lrl, Lrr]],[{R,black},Rl,Rr]];
-case1([{G,Gc}, [{L,red}, Ll, Lr],[{R,red}, [{Item,red},Rll,Rlr]], Rr],
+    [{G,red}, [{L,black}, Ll, [{Item, red}, Lrl, Lrr]],[{R,black},Rl,Rr]];
+case1([{G,_}, [{L,red}, Ll, Lr], [{R,red},[{Item,red},Rll,Rlr], Rr]],
 	Item) ->
-    [{G,Gc}, [{L,black}, Ll, Lr],[{R,black}, [{Item,red},Rll,Rlr]], Rr];
-case1([{G,Gc}, [{L,red}, Ll, Lr],[{R,red}, Rl, [{Item,red},Rrl,Rrr]]],       
+    [{G,red}, [{L,black}, Ll, Lr],[{R,black}, [{Item,red},Rll,Rlr], Rr]];
+case1([{G,_}, 
+       [{L,red}, Ll, Lr],
+       [{R,red}, Rl, [{Item,red},Rrl,Rrr]]],       
 	Item) ->
-    [{G,Gc}, [{L,black}, Ll, Lr],[{R,black}, Rl, [{Item,red},Rrl,Rrr]]];
+    [{G,red}, [{L,black}, Ll, Lr],[{R,black}, Rl, [{Item,red},Rrl,Rrr]]];
 case1(Tree, Item) ->
     case2(Tree, Item).
 
@@ -60,21 +70,15 @@ case1(Tree, Item) ->
 %    B    =>    B      and         B   =>      B
 %     \        /                  /             \
 %      C      C                  C               C 
-case2( [{G, _}, [{L,red}, Ll, [{Item, red}, Lrl, Lrr]], [{R,Rc}, Rl, Rr]] ,
+case2( [{G, _}, [{L,red}, Ll, [{Item, red}, Lrl, Lrr]], R] ,
        Item) ->
-    case3([{G, red}, 
-	   [{Item, black}, [{L, red}, Ll, Lrl], Lrr ],
-	   [{R, Rc}, Rl, Rr]
-	  ], L);
-case2( [{G, _}, [{L, Lc}, Ll, Lr], [{R, red}, [{Item,red}, Rll, Rlr]], Rr],
+    case3([{G, red}, [{Item, black}, [{L, red}, Ll, Lrl], Lrr ], R], Item);
+case2( [{G, _},L,[{R, red}, [{Item,red}, Rll, Rlr], Rr]],
        Item) ->
-    case3([{G, red}, 
-	   [{L, Lc}, Ll, Lr], 
-	   [{Item, black}, Rll, [{R, red}, Rlr, Rr]]
-	  ], R);
+    case3([{G, red}, L,  [{Item, black}, Rll, [{R, red}, Rlr, Rr]]], Item);
+
 case2( Tree, Item)  ->
     case3(Tree, Item).
-
 
 % Converting Zig-Zig to Bent Arrow shape
 %        A                     A
@@ -82,11 +86,19 @@ case2( Tree, Item)  ->
 %      B    =>   / \     and     B    =>   / \ 
 %     /         C   A             \       A   C
 %    C                             C
-case3([{G, red}, 
-	   [{Item, black}, [{L, red}, Ll, Lrl], Lrr ],
-	   [{R, Rc}, Rl, Rr]
-	  ], R) ->
-    
+%case3([{G, red}, 
+%	   [{Item, black}, [{L, red}, Ll, Lrl], Lrr ],
+%	   [{R, Rc}, Rl, Rr]
+%	  ], R) ->
+
+case3([{G, red}, [{Item, black}, L, Lr ], R], Item) ->    
+    [{Item, black}, L, [{G, red}, Lr, R]];
+
+case3([{G, red}, L, [{Item, black}, Rl, R]], Item) ->
+    [{Item, black}, [{G, red}, L, Rl], R];
+
+case3(Tree, _)->   
+    Tree.
 
 
     
