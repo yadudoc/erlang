5613cef5af9c6d69a228d1af4553a1531aec478e
Yadu Nand B <yadudoc1729@gmail.com>
2011-03-10 19:37:47 +0530

    modified:   tree_print.erl

--- tree_print.erl
+++ tree_print.erl
@@ -8,7 +8,7 @@
 	]).
 
 prettyprint(Tree)->
-    pp(Tree, " "),
+    pp(Tree, "  "),
     io:format("~n").
 
 pp(null,_) ->
@@ -17,15 +17,18 @@ pp(null,_) ->
 pp([{Node, red}, Left, Right], Space) ->
     io:format("~pr -----> ",[Node]),
     pp(Left, string:concat(Space,"        ")),
-    io:format("~p +----> ",[Space]),
-    pp(Right,string:concat(Space,"       "));
+    io:format(Space),
+    io:format("   +------> "),
+    pp(Right,string:concat(Space,"        "));
 pp([{Node, black}, Left, Right], Space) ->
     io:format("~pb -----> ",[Node]),
     pp(Left, string:concat(Space,"        ")),
-    io:format("~p +----> ",[Space]),
-    pp(Right,string:concat(Space,"       "));
+    io:format(Space),
+    io:format("   +------> "),
+    pp(Right,string:concat(Space,"        "));
 pp([Node, Left, Right], Space) ->
     io:format("~p -----> ",[Node]),
     pp(Left, string:concat(Space,"        ")),
-    io:format("~p +----> ",[Space]),
-    pp(Right,string:concat(Space,"       ")).
+    io:format(Space),
+    io:format("   +------> "),
+    pp(Right,string:concat(Space,"        ")).
