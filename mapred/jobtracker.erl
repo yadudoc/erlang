c692ae965df2b7b17ae6ca4a1582c5f9b928a8e8
Yadu Nand B <yadudoc1729@gmail.com>
2011-04-14 17:32:59 +0530

    Mappers are done, tested

--- mapred/jobtracker.erl
+++ mapred/jobtracker.erl
@@ -1,13 +1,13 @@
 -module(jobtracker).
 -export([
+	 start/2,
 	 broadcast/1,
-	 broadcaster/2,
 	 broadcaster/3,
 	 submit_job/0,
 	 submit_job/5,
 	 submit_job/6,
 	 mapper/2,
-	 jtracker/1
+	 jtracker/5
 	 ]).
 
 
@@ -19,14 +19,24 @@ broadcast(Reg_name)->
 % Repeatedly broadcast every set duration to identify new nodes
 % We only check for new nodes. 
 % NOTE : WE ARE NOT HANDLING NODES GOING DOWN HERE
-broadcaster(Reg_name, Time)->    
+start(Reg_name, Time)->    
     %% spawn the jobtracker with registered name mr_jobtracker
-    register( 'jTracker', spawn(jobtracker, jtracker, [5000]) ),
     register( 'bCaster' , spawn(jobtracker,broadcaster, 
-				    [Reg_name, Time, []])).
+				    [Reg_name, Time, []])),
+    io:format("~n Jobtracker: Broadcaster is Up!"),
+    
+    register( 'jTracker', spawn(jobtracker, jtracker, 
+				[Reg_name, 5000, [], 
+				 [ [[],[]], [[],[]], [] ], 
+				 []
+				]
+			       ) ),
+    io:format("~n Jobtracker: Jobtracker main thread is Up!").
+    
     
 
 broadcaster(Reg_name, Time, Good_old) ->
+
     {Good_new, _} = broadcast(Reg_name),
     New_node = Good_new -- Good_old,
     if 
@@ -39,6 +49,10 @@ broadcaster(Reg_name, Time, Good_old) ->
     %% The following is a lame sleep implementation
     %% we want the broadcast only in a few minutes
     receive
+	{global_die} ->
+	    rpc:sbcast(Reg_name, {global_die}),
+	    io:format("~n Broadcaster dying..");
+		      
 	{die} ->
 	    io:format("~n Broadcaster dying..")
     after Time ->
@@ -48,22 +62,68 @@ broadcaster(Reg_name, Time, Good_old) ->
 
 %% listens and handles all notifications on node discovery
 %%  status, node lost etc.
-jtracker(Time) ->
+%% Functions is of type [ [Mapper,Inputpatte], [Reducer,N_reducers] ]
+%% Files is of type [ [[ Input_files_todo ], [Input_files_done]],
+%%                    [[ Inter_files_todo ], [Inter_files_done]],  
+%%                    [ Output_file_done ]]
+%% Nodes is a list of ready/active nodes
+
+jtracker(Reg_name, Time, Functions, Files, Nodes) ->
+    %%io:format("~n Node(s) ->  ~p",[Nodes]),
     receive 
 	{die} ->
 	    io:format("~n jTracker exiting...");
+
+	%% Bit of a security issue right here.
+	%% anybody could technically send a global_die message
+	{global_die} ->
+	    bCaster ! {global_die},
+	    io:format("~n Jobtracker dying..");
+	
 	{no_new_node} ->
-	    jtracker(Time);
+	    jtracker(Reg_name, Time, Functions, Files, Nodes);
+	
 	{new_node, New_node} ->
 	    io:format("~n New node(s) found! ->  ~p",[New_node]),
-	    jtracker(Time);
+	    jtracker(Reg_name, Time, Functions, Files, Nodes++New_node );
+	
+	
+	{files,Node,Input_files} ->
+	    [ [ITodo,IDone],Inter,Result ] = Files,
+	    jtracker(Reg_name, Time, Functions, 
+		     [
+		      [[[Node, Ifile] || Ifile <- Input_files]++ITodo, IDone],
+		      Inter,
+		      Result], Nodes);	       
+
+
+	%% Allow new requests only when the job is completed !	
+	{job_request, Mapfunc, Inp_pattern, Num_mappers, 
+	 Redfunc, Num_reducers} when Functions =:= [] ->
+	    io:format("~n Requesting map job at nodes ~p",[Nodes]),
+
+	    rpc:sbcast(Nodes, Reg_name,
+		       {job_tracker_map, Mapfunc, Inp_pattern, 
+			Num_mappers, Num_reducers}
+		      ),
+	    jtracker(Reg_name,Time, [Mapfunc, Redfunc], Files, Nodes);
+	
+	
+	
+	{mapper_result_success, Node, Filename, Inter_files} ->
+	    [ [InpTodo,InpDone],[IntTodo,IntDone],Result ] = Files,
+	    Temp = [ [Node,F] || F <- Inter_files],
+	    jtracker(Reg_name, Time, Functions,
+		     [ [InpTodo -- [Node,Filename],InpDone++[Node,Filename]],
+		       [IntTodo ++ Temp, IntDone], Result
+		     ], Nodes);       	    
+
+	{mapper_complete, Node} ->
+	    io:format("~n Mapping complete on node: ~p ",[Node]);
+
 	Any ->
 	    io:format("~n jTracker received message ~p",[Any]),
-	    jtracker(Time)
-
-    after Time ->
-	    io:format("~n jTracker wait looping"),
-	    jtracker(Time)
+	    jtracker(Reg_name, Time, Functions, Files, Nodes)
     end.
 	        	    	          
 
@@ -85,7 +145,7 @@ submit_job() ->
 % by default registered server name is "miley"
 submit_job(Mapfunc, N_mappers, Redfunc, N_reducers, Data_ids) ->
     submit_job(Mapfunc, N_mappers, Redfunc, 
-	       N_reducers, Data_ids, miley).
+	       N_reducers, Data_ids, 'Miranda').
 
 submit_job(Mapfunc, N_mappers, Redfunc, N_reducers, Data_ids, Server_id) ->
     
--- mapred/tasktracker.erl
+++ mapred/tasktracker.erl
@@ -2,8 +2,22 @@
 -export([
 	 start/2,
 	 task_tracker/2,
-	 mworker/4
+	 mapper/6,
+	 mworker/4,
+	 yoyo/0,
+	 yoyo/1
 	 ]).
+
+yoyo()->
+    register(yoyo, spawn(tasktracker,yoyo,[loop])).
+
+yoyo(loop) ->
+    receive 
+	_ ->
+	    yoyo(loop)
+    end.
+
+
 
 start(Reg_name, Job_tracker) ->
     register( Reg_name, spawn(tasktracker, task_tracker, 
@@ -14,12 +28,12 @@ start(Reg_name, Job_tracker) ->
 %% pings the main server which has the jobtracker 
 %% NOTE: THE NODE RUNNING JOBTRACKER MUST BE NAMED GIVEN AS ARG
 task_tracker(discovery, Job_tracker) ->
-    io:format("~ntask_tracker : Discovery phase, Attempting contact with ~p",
+    io:format("task_tracker : Discovery phase, Attempting contact with ~p~n",
 	     [Job_tracker]),
     case net_adm:ping(Job_tracker) of
 	pong ->
-	    io:format("~ntask_tracker : Job_tracker found "),
-	    task_tracker(discovered, [{jobtracker, Job_tracker}|[ready]] );
+	    io:format("task_tracker : Job_tracker found~n"),
+	    task_tracker(discovered, [{jobtracker, Job_tracker}|[[ready]]] );
 	pang ->
 	    receive
 		%% if the old Job_tracker dies out, we may have these nodes
@@ -50,32 +64,42 @@ task_tracker(discovered, Status) ->
 	    task_tracker(discovered, Status );
 
 	
+	{global_die} ->
+	    [ [_] | [[Stat]|_] ] = Status,
+	    if 
+		Stat =/= ready ->
+		    mapper ! {die}
+	    end,	          	    
+	    io:format("~ntask_tracker dying.. ~n");
+	    
+
 	%% Request for spawning mappers
 	%% 1. Spawn off mappers
 	%% 2. Update status loopback
 	{job_tracker_map, Mapperfunc, Pattern, Num_mappers, Num_reducers}
 	->
-	    [JTnode|[[Stat]|_]] = Status,
+	    [{jobtracker, JTnode}|[[Stat]|_]] = Status,
 	    case Stat of  
 		ready ->		    
 		    Input_files = filelib:wildcard(Pattern),    
+		    rpc:sbcast([JTnode],jTracker,{files,node(),Input_files}),
 		    register('mapper', spawn(tasktracker, mapper, 
 					     [Mapperfunc, 
-					      [Input_files,[],[]],
-					      Num_mappers,
-					      0,
+					      [Input_files,[],[],[]],
+					      Num_mappers,	      
 					      Num_reducers,
-					      self()
+					      self(),
+					      0
 					     ])),
-		    
+		    io:format("~n Received Map Job~n"),
 		    task_tracker(discovered,     %% Task_tracker status  
-				 [JTnode ,       %% Job_tracker details    
+				 [{jobtracker,JTnode}, %% Job_tracker details   
 				  [active],      %% Current status
 				  [Input_files, [], []], %% Input files 
 				  [[], [], []],  %% Intermediate files
 				  []             %% Result files
 				 ]
-				 );
+				);
 		_ ->
 		    io:format("~nIllegal attempt : Mappers in use")
 	    end;
@@ -93,35 +117,60 @@ task_tracker(discovered, Status) ->
 	%% 
 	{mapper_result_success, Filename, Inter_files} 
 	->
-	    [JTnode, 
+	    %%io:format("~n File:~p processed, New file-> ~p",
+	%%	      [Filename, Inter_files]),
+	    [{jobtracker,JTnode}, 
 	     [_], 
 	     [Input_mfiles, Done_mfiles, Bad_mfiles],
 	     [Int_rfiles, Done_rfiles, Bad_rfile],
 	     Resultfiles
-	     ] = Status,
+	    ] = Status,
+	    rpc:sbcast([JTnode],jTracker,{mapper_result_success, 
+					  node(), Filename, Inter_files}),
 	    task_tracker(discovered,   
-			 [JTnode ,     
+			 [{jobtracker,JTnode} ,     
 			  [active],    
-			  [Input_mfiles -- Filename, 
+			  [Input_mfiles -- Filename,
 			   Done_mfiles ++ Filename,
 			   Bad_mfiles],
 			  [Int_rfiles ++ Inter_files, Done_rfiles, Bad_rfile],
 			  Resultfiles
 			 ]
-			),
+			);	
 	    
-	    ok;
-	
-	{mapper_result_failure, Filename} 
+		   
+	{mapper_complete}
 	->
-	    [JTnode, 
+	    [{jobtracker,JTnode}, 
 	     [_], 
 	     [Input_mfiles, Done_mfiles, Bad_mfiles],
 	     [Int_rfiles, Done_rfiles, Bad_rfile],
 	     Resultfiles
-	     ] = Status,
+	    ] = Status,
+	    rpc:sbcast([JTnode], jTracker, {mapper_complete, node()}),
+	    io:format("~n Mapping complete! "),
 	    task_tracker(discovered,   
-			 [JTnode ,     
+			 [{jobtracker,JTnode} ,     
+			  [active],    
+			  [Input_mfiles, Done_mfiles, Bad_mfiles],
+			  [Int_rfiles , Done_rfiles, Bad_rfile],
+			  Resultfiles
+			 ]
+			);	
+	    
+	
+	
+
+	{mapper_result_failure, Filename} 
+	->
+	    [{jobtracker,JTnode},
+	     [_], 
+	     [Input_mfiles, Done_mfiles, Bad_mfiles],
+	     [Int_rfiles, Done_rfiles, Bad_rfile],
+	     Resultfiles
+	    ] = Status,
+	    task_tracker(discovered,   
+			 [{jobtracker,JTnode},
 			  [active],    
 			  [Input_mfiles -- Filename, 
 			   Done_mfiles , 
@@ -130,8 +179,9 @@ task_tracker(discovered, Status) ->
 			  Resultfiles
 			 ]
 			),
-	    
-	    ok;
+	    rpc:sbcast([JTnode],jTracker,{mapper_result_success, 
+				      node(), Filename});
+		   
 
 	
 	%% Update status and loop task_tracker
@@ -140,58 +190,85 @@ task_tracker(discovered, Status) ->
 	->
 
 	    ok;	    
+	%% Global abort 
+	%% kill all jobs. Leave everything as it is.
+	%% Go back to the discovery phase
+	{global_abort} ->
+	    [{jobtracker, JTracker}|[ [State] |_ ]] = Status,
+	    mapper ! {die},
+	    task_tracker(discovery, JTracker);
+
 	{die} ->
 	    io:format("task_tracker: Exiting...")
-    after 12000 ->
-	    [{jobtracker, Job_tracker}|_] = Status,
-	    io:format("~nNo broadcast from ~p~n",[Job_tracker]),
-	    task_tracker(discovery, Job_tracker)
+    after 12000 ->	    	  
+	    [{jobtracker, JTracker}|[ [State] |_ ]] = Status,
+	    case State of
+		active ->		    
+		    task_tracker(discovered, Status);
+		ready ->
+		    task_tracker(discovery, JTracker);
+		done ->
+		    task_tracker(discovery, JTracker)
+	    end
     end.
+
+
+
+
+mapper(_, [[], [], _, _], _, _, Task_tracker, _) ->
+%%    io:format("Mapper complete"),
+    Task_tracker ! {mapper_complete};
 
 
 %% mapper organises the mapping operation
 %% ensures that only the specified number of mappers are running
-mapper(Mapperfunc, [[Todo],[Processing],[Done], [Badfile]],
-       Num_mappers, Num_reducers, Task_tracker, WorkerCount) ->    
-
+mapper(Mapperfunc, [Todo, Processing, Done, Badfile],
+       Num_mappers, Num_reducers, Task_tracker, WorkerCount) ->
+  
     Me = self(),
     if
-	WorkerCount < Num_mappers ->
+%%	Todo =:= [] , Processing =:= [] ->
+%%	    exit(done);
+	    
+	WorkerCount < Num_mappers , Todo =/= [] ->
 	    [H|_] = Todo,
 	    spawn(tasktracker, mworker, [Mapperfunc, H, Num_reducers, self()]),
 	    mapper(Mapperfunc, 
-		   [[Todo]--[H],
-		    [Processing]++[H],
-		    [Done], 
-		    [Badfile]
+		   [Todo--[H],
+		    Processing++[H],
+		    Done, 
+		    Badfile
 		   ],
-		   Num_mappers, Num_reducers, Task_tracker, WorkerCount+1)
+		   Num_mappers, Num_reducers, Task_tracker, WorkerCount+1);
+	true ->
+	    ok
     end,	    
 
     receive
 	{mapper_success, Me, Inp_file, Int_files} ->	    
-	    Task_tracker ! {mapper_result, Inp_file, Int_files},
+	    io:format("Mapper: ~p done~n",[Inp_file]),
+	    Task_tracker ! {mapper_result_success, Inp_file, Int_files},
 	    mapper(Mapperfunc, 
-		   [[Todo],
-		    [Processing]--[Inp_file],
-		    [Done]++[Inp_file], 
-		    [Badfile]
+		   [Todo,
+		    Processing--[Inp_file],
+		    Done++[Inp_file], 
+		    Badfile
 		   ],
 		   Num_mappers, Num_reducers, Task_tracker, WorkerCount-1);
+
 	{mapper_failure, Me, Inp_file} ->
-	    Task_tracker ! {mapper_result, Inp_file},
+	    Task_tracker ! {mapper_result_failure, Inp_file},
 	    mapper(Mapperfunc, 
-		   [[Todo],
-		    [Processing]--[Inp_file],
-		    [Done],
-		    [Badfile]++[Inp_file]
+		   [Todo,
+		    Processing--[Inp_file],
+		    Done,
+		    Badfile ++[Inp_file]
 		   ],
-		   Num_mappers, Num_reducers, Task_tracker, WorkerCount-1)
-	    
+		   Num_mappers, Num_reducers, Task_tracker, WorkerCount-1);
+	{die} ->
+	    ok		
     end.
     
-    
-
 %% mworker handles a single map job at a time
 %% 1. It reads in lines from a specified file
 %% 2. Applies the Mapperfun on each line read in
@@ -201,6 +278,7 @@ mapper(Mapperfunc, [[Todo],[Processing],[Done], [Badfile]],
 %% 5. The sublists which go to different reducers, are written to different
 %%        files, with the reducer number as the prefix
 mworker(Mapperfun, Filename, Num_reducers, Mapper_id ) ->    
+%%    io:format("~n mworker invoked for file -> ~p",[Filename]),
     case fileio:readline(Filename) of
 	{ok, Contents} ->
 	    
@@ -228,16 +306,22 @@ mworker(Mapperfun, Filename, Num_reducers, Mapper_id ) ->
 	    [Fname | _ ] = string:tokens(Filename,"."),
 	    
 	    
-	    Intermediates = lists:foldl( fun(X, Acc) ->	
-       				 [Num] = io_lib:format("~p",[Acc]),
-				 Out = Num ++ "_" ++ Fname ++ ".int",
-				 %% io:format("~p",[Out]),
+
+	    lists:foldl( fun(X, Acc) ->	
+				 [Num] = io_lib:format("~p",[Acc]),
+				 Out = Fname ++"_" ++Num++ ".int" ,
+%%				 io:format("~n~p",[Out]),
 				 fileio:writelines(write, Out, X),
 				 1+Acc
-				 end, 0, Lists),
+			 end,
+			 0, 
+			 Lists),	    
+	    
+	    %% slightly crappy way of doing this
+ 	    Intermediates = filelib:wildcard(Fname++"*.int"),
 	    %% Send reply to mapper of successful completion
 	    Mapper_id ! {mapper_success, Mapper_id, Filename, Intermediates};
-		
+	
 	%% we are ignoring the reason for now. Till better reason handling is
 	%% implemented
 	{error, _} ->
